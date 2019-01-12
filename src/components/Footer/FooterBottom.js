f7f72ff83cd25039eb1d08efe5dea09866583ecc
Ivo <ikovacevic96k@gmail.com>
2019-01-12 19:27:59 +0100

    login part 2, added link to prijava page

--- src/components/Footer/FooterBottom.js
+++ src/components/Footer/FooterBottom.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Container from "../Container";
+import { navigate } from "@reach/router";
 
 const FooterBottom = () => {
   return (
     <div className={styles.FooterBottomContainer}>
       <Container>
-        <p>Copyright © 2018-2019 DiveIn web shop. All Rights Reserved.</p>
+        <p onClick={() => {navigate('prijava')}}>Copyright © 2018-2019 DiveIn web shop. All Rights Reserved.</p>
       </Container>
     </div>
   );
--- src/pages/admin-page.jsx
+++ src/pages/admin-page.jsx
@@ -9,9 +9,16 @@ class AdminPage extends Component {
     }
   };
   render() {
+      const user = JSON.parse(localStorage.getItem('user'));
     return (
       <>
-        <button onClick={this.onLogOutClick}>Log out</button>
+      {user && <div>
+
+          <button onClick={this.onLogOutClick}>Log out</button>
+
+          <h1>Hello {user.name} {user.surname}</h1>
+      </div>
+    }
       </>
     );
   }
